49cc097b210569f6e697390cbaaaa8bde2ab81ac
Char Stiles <ch4@mit.edu>
2024-09-07 15:11:06 -0400

    removed camera stuff

--- public/index.js
+++ public/index.js
@@ -55,60 +55,8 @@ if (window.isProduction && window.location.protocol !== "https:") {
   window.location = "https://" + window.location.hostname;
 }
 
-class Camera {
-  constructor() {
-    this.video = document.createElement("video");
-    this.video.setAttribute("muted", true);
-    this.video.setAttribute("playsinline", false);
-
-    this.selfie = false;
-
-    this.audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-  }
-
-  _startCapture() {
-    return navigator.mediaDevices
-      .getUserMedia({
-        audio: true,
-        video: false,
-      })
-      .then((stream) => {
-        this.stream = stream;
-        var source = this.audioCtx.createMediaStreamSource(stream);
-
-        this.analyser = this.audioCtx.createAnalyser();
-        this.analyser.smoothingTimeConstant = 0.9;
-        this.analyser.fftSize = FFT_SIZE;
-        source.connect(this.analyser);
-      });
-  }
-  init() {
-    this._startCapture();
-    return this._startCapture();
-  }
-  flip() {
-    this.selfie = !this.selfie;
-    this._startCapture();
-  }
-}
-
 let button = document.querySelector("button");
-let camera;// new Camera();
-//document.querySelector("body").appendChild(camera.video);
 
-// document.addEventListener("click", function (e) {
-//   camera
-//     .init()
-//     .then(start)
-//     .catch((e) => console.error(e));
-// });
-
-// (function () {
-//   camera
-//     .init()
-//     .then(start)
-//     .catch((e) => console.error(e));
-// })();
 
 function start() {}
 
@@ -174,7 +122,6 @@ function init() {
   timeSinceStuck = Date.now();
   // SOCKET IO
   socket = io();
-  // addSendButton()
 
   var editorContainer = document.getElementById("editor");
   editor = CodeMirror(editorContainer, {
